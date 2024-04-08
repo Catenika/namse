f3d774c60a79cc0a7776c9451f77063b24affc7f
Catenika <139275513+Catenika@users.noreply.github.com>
2024-04-08 10:54:32 +0300

    Update colors.ts

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,9 +2,9 @@ import { Colors } from '@chakra-ui/react'
 
 export const colors: Colors = {
   'light-container': '#FFFFFF',
-  'light-bg': '#F3F4F6',
-  'light-theme': '#00B5D8',
-  'dark-container': '#2A334C',
-  'dark-bg': '#171D30',
-  'dark-theme': '#FFFF00',
+  'light-bg': '#F3F3F6',
+  'light-theme': '#01B5D8',
+  'dark-container': '#2A434C',
+  'dark-bg': '#172D30',
+  'dark-theme': '#FFF000',
 }
